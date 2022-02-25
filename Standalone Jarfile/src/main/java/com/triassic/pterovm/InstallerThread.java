c28d820ea107b020c806ec301b7405f9f57b089e
RealTriassic <techbyteofficial9@gmail.com>
2022-02-25 12:48:37 +0200

    Add support for custom.sh files.

--- Standalone Jarfile/src/main/java/com/triassic/pterovm/InstallerThread.java
+++ Standalone Jarfile/src/main/java/com/triassic/pterovm/InstallerThread.java
@@ -1,13 +1,44 @@
 package com.triassic.pterovm;
 
+import java.io.File;
+
 public class InstallerThread extends Thread {
     public void run() {
         try {
 
-            // This is the command that will be executed on server start-up.
-            String[] startCommand = { "bash", "installer.sh" };
+            // Initialize the startCommand string to later modify it in an if-else statement.
+            String[] startCommand;
 
-            // Create the ProcessBuilder for our installerProcess.
+            // Check if custom.sh file exists.
+            File customScript = new File("custom.sh");
+
+            if (customScript.exists()) {
+
+                System.out.println("Found custom.sh file, loading from custom.sh...");
+                System.out.flush();
+
+                // Send special ANSI codes to center cursor, also known as "clearing console screen".
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+
+                // Define the new startCommand for loading custom.sh
+                startCommand = new String[] { "bash", "custom.sh" };
+
+            } else {
+
+                System.out.println("No custom.sh file was found, loading from installer.sh...");
+                System.out.flush();
+
+                // Send special ANSI codes to center cursor, also known as "clearing console screen".
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+
+                // Define the new startCommand for loading installer.sh
+                startCommand = new String[] { "bash", "installer.sh" };
+
+            }
+
+            // Create the ProcessBuilder for our script file.
             ProcessBuilder installerProcess = new ProcessBuilder(startCommand);
 
             // Inherit installerProcess's input and output and block the thread to "wait" for the file to "finish" executing.
--- Standalone Jarfile/src/main/java/com/triassic/pterovm/Main.java
+++ Standalone Jarfile/src/main/java/com/triassic/pterovm/Main.java
@@ -9,38 +9,35 @@ import java.nio.channels.ReadableByteChannel;
 public class Main {
     public static void main(String[] args) {
 
-            // Send special ANSI codes to center cursor, also known as "clearing console screen".
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
+        // Send special ANSI codes to center cursor, also known as "clearing console screen".
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
 
-        try {
+        // Define the name for any custom scripts that can run instead of the normal installer.sh.
+        File customScript = new File("custom.sh");
 
-            // Download the latest Installer.sh file off of the source.
-            File installerFile = new File("installer.sh");
-            URL fetchFile = new URL("https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/installer.sh");
+        // Download installer.sh if custom.sh does not exist.
+        if (!customScript.exists()) {
+            try {
 
-            // Check if the Installer.sh file exists and if it does, skip the download, otherwise, download the file.
-            if (!installerFile.exists()) {
+                // Download the latest Installer.sh file off of the source.
+                URL fetchFile = new URL("https://raw.githubusercontent.com/afnan007a/Ptero-vm/main/installer.sh");
 
                 System.out.println("Downloading the Installer.sh file...");
                 System.out.flush();
 
                 ReadableByteChannel readableByteChannel = Channels.newChannel(fetchFile.openStream());
 
-                try (FileOutputStream fos = new FileOutputStream(installerFile)) {
+                try (FileOutputStream fos = new FileOutputStream("installer.sh")) {
                     fos.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
                 }
 
-            } else {
-                System.out.println("Skipping Installer.sh file download, file exists.");
+            } catch (Exception error) {
+                System.out.println("An error occurred whilst downloading installer.sh.");
                 System.out.flush();
+
+                error.printStackTrace();
             }
-
-        } catch (Exception error) {
-            System.out.println("An error occurred whilst downloading installer.sh.");
-            System.out.flush();
-
-            error.printStackTrace();
         }
 
         try {
